ce4021a7efffabc575e03795dd470426d13c0f14
Dattatray Kale <way2datta@gmail.com>
2018-07-05 08:12:57 +0530

    Production webpack configuration

---
+++ buildScripts/distServer.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
+import compression from 'compression';
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('dist'));
+app.use(compression());
+
+app.get('/', function(request, response) {
+  response.sendFile(join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, function(error) {
+  if(error) {
+    console.log(error);
+  } else {
+    open('http://localhost:'+port);
+  }
+});
